fe6d7fd4a1aa0905bd5b2a8f5cf7ab86bd39e208
Cr1stopher636 <ricardo0640@outlook.com>
2026-08-14 17:33:10 -0600

    add: objects

---
+++ modulos/archivo.js
@@ -0,0 +1,18 @@
+const usuario = { 
+    nombre: "ricardo", rol: "programador",
+    edad: 23,
+    profesión: "Ingeniero en software"
+};
+
+const usuario2 = { 
+    nombre: "andrea", rol: "UX",
+    edad: 21,
+    profesión: "Licenciada en Mercadología"
+};
+
+const usuario3 = { 
+    nombre: "Renata", rol: "IU",
+    edad: 19,
+    profesión: "Ingeniera industrial"
+};
+module.exports = {usuario, usuario2, usuario3}; // Exporta el objeto
