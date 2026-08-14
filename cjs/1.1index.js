21894d829687709348e475070aef61f1a10c342c
Cr1stopher636 <ricardo0640@outlook.com>
2026-08-14 12:13:52 -0600

    add: CommonJS

---
+++ cjs/1.1index.js
@@ -0,0 +1,8 @@
+// Common JS require module
+const { rest } = require('./1.2suma')
+const { sum } = require('./1.2suma')
+
+console.log(sum(1, 2));
+console.log(rest(1, 2));
+
+
---
+++ cjs/1.2suma.js
@@ -0,0 +1,11 @@
+// Common JS module exports
+
+module.exports = {
+    rest: function rest(a, b) {
+        return a - b
+    }, 
+    sum: function sum(a, b) {
+        return a + b
+    }
+}
+
