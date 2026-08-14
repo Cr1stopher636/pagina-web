520ec3755ecbe0364db428d01c8d7452e52ed18a
Cr1stopher636 <ricardo0640@outlook.com>
2026-08-14 12:14:07 -0600

    add: ES modules

---
+++ mjs/index.mjs
@@ -0,0 +1,8 @@
+// js -> Common JS
+// mjs -> ES modules
+// .cjs -> para utilizar Common JS
+
+import { sum, rest, mult } from "./suma.mjs";
+console.log(sum(1, 2));
+console.log(rest(1, 2));
+console.log(mult(1, 2));
