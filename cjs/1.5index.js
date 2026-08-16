7253d98ecea71d54175ac287a2465e5af1473c56
Cr1stopher636 <ricardo0640@outlook.com>
2026-08-16 14:44:54 -0600

    add: mult

---
+++ cjs/1.5index.js
@@ -0,0 +1,3 @@
+const { mult } = require ('./1.6mult')
+
+console.log(mult(9, 5));
---
+++ cjs/1.6mult.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mult:
+        function multiplicacion(a, b) {
+            return a * b
+        }
+}
+
