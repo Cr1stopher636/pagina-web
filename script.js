f46ef02065dd1856d51ea387589bf56c47a1f702
Cr1stopher636 <ricardo0640@outlook.com>
2026-08-31 14:37:57 -0600

    add: diccionario

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-const nombre = require('./cjs/1.2suma')
+const diccionario = ['nombre','ricardo','edad', 23];
+diccionario.forEach(element => {
+    console.log(element);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(diccionario['nombre']);
+
+// const fs = require('node:fs');
+
+// const variable = fs.readFileSync('./archivo32.txt');
+// console.log(variable.toString());
