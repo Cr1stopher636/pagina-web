28706d6a2be1c27ded35222f1f93af3b0d3128a2
Cr1stopher636 <ricardo0640@outlook.com>
2026-08-31 14:37:44 -0600

    change: practica

---
+++ practica.js
@@ -0,0 +1,64 @@
+const fs = require('node:fs/promises');
+
+const promesa = new Promise((resolve, reject) => {
+    console.log('Se leyo el archivo')
+    console.log()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// .then(files => {
+//     files.forEach(files => {
+//         console.log(files);
+//     })
+// })
+// .catch(err => {
+//     if (err) {
+//         console.log('Error al leer el directorio', err)
+//         return;
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
