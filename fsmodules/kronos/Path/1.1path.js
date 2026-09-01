72232d6fef7a14c2de37f9d326960a1251cdcc70
Cr1stopher636 <ricardo0640@outlook.com>
2026-08-31 21:51:52 -0600

    add: const path

---
+++ fsmodules/kronos/Path/1.1path.js
@@ -0,0 +1,19 @@
+const path = require('node:path')
+
+// Barra separadora de carpetas segun SO
+// console.log(path.sep);
+
+// Unir rutas con path.join
+// const filepath = path.join('content', 'subfolder', 'text')
+// console.log(filepath);
+
+
+// const base = path.basename('web/fsmodules/kronos/Path')
+// console.log(base);
+
+// const filename = path.basename('kronos/Path', '.txt')
+// console.log(filename);
+
+// const extension = path.extname('./tumblr.jpg')
+// console.log(extension);
+
