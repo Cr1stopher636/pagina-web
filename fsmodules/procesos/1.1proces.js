901c68d91d1ae047cb4747a840e1be1a698b62cb
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-02 09:14:22 -0600

    change: argv

---
+++ fsmodules/procesos/1.1proces.js
@@ -0,0 +1,24 @@
+// Variable de entorno
+// console.log(process.env.PEPITO);
+
+// argumentos de entrada
+console.log(process.argv);
+
+// controlar el proceso y salida
+// process.exit(1);
+
+// controlar eventos del proceso
+// process.on('exit', () => {
+
+// })
+
+// console.log(process);
+
+// current working directory
+// console.log(process.cwd());
+  
+
+
+
+
+
