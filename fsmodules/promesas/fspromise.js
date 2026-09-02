58b3c9036b84b587a0e72db47c2090d325f74bb4
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-02 11:12:32 -0600

    new: changes

--- fsmodules/promesas/fspromise.js
+++ fsmodules/promesas/fspromise.js
@@ -1,13 +1,49 @@
 // Promesa explícita
-const variable = new Promise((resolve, reject) => {
-    const exito = true;
+// const variable = new Promise((resolve, reject) => {
+//     const exito = true;
 
-    if (exito) {
-        resolve('Se completo la promesa con éxito')
-    } else {
-        reject('No se completo la promesa')
-    }
-});
+
+//     if (exito) {
+//         resolve('Se completo la promesa con éxito')
+//     } else {
+//         reject('No se completo la promesa')
+//     }
+// });
+
+// const fs = require('node:fs/promises')
+// async function init() {
+//     const files = await fs.readFile('./1.3promise.js', 'utf-8');
+//     console.log('Este es el resultado: --->', files);
+// }
+
+// init();
+
+
+// const fs = require('node:fs')
+// const files = fs.readFile('./1.2promise.js', 'utf-8', (err, text) => {
+//     if (err) {
+//         console.error('Salio mal', err);
+//         return;
+
+//     }
+//     console.log('Este es el resultado Papi: --->', text);
+// });
+
+
+
+
+// const fs = require('node:fs/promises');
+
+// async function init() {
+//     const files = await fs.readFile('./1.3promise.js', 'utf-8')
+//     console.log(files);
+// }
+
+// init();
+
+
+
+
 
 // variable //Para consumir una promise
 //     .then((mensaje) => {
