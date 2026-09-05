d02fe9713de74f8e71bcbf3e76faaaec8196868f
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-05 16:31:03 -0600

    add: callbacks asincronia

---
+++ fsmodules/procesos/1.6advance.js
@@ -0,0 +1,21 @@
+
+const fs = require('node:fs');
+
+console.log('Este es el primer archivo ------');
+fs.readFile('./1.1proces.js', 'utf-8', (err, text) => {
+    console.log(text)
+})
+
+console.log('Este es el primer archivo ------');
+fs.readFile('./1.1proces.js', 'utf-8', (err, text) => {
+    console.log(text)
+})
+
+// fs.readFile('./1.5advance.js', 'utf-8', (err, text) => {
+//     console.log(text);
+
+// });
+
+
+
+
