9832417e370886c8bb89ba9b1c387fbf571a6a16
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-05 14:43:50 -0600

    new files

--- fsmodules/procesos/1.5advance.js
+++ fsmodules/procesos/1.5advance.js
@@ -1,16 +1,19 @@
-
 const fs = require('node:fs/promises')
 
 const variable = process.argv[2] ?? 'No se encontro la variable de entorno';
+console.log(variable);
 
 
-fs.stat('./1.1proces.js')
-.then(file => {
-    console.log(file)
-})
-.catch(error => {
-    console.error(error)
-});
+// fs.readdir('./')
+//     .then(files => console.log(files));
+   
+// console.log('Este es: -->', process.cwd());
+
+// .then(file => {
+//     console.log(file)
+// .catch(error => {
+//     console.error(error)
+// });
   
 
 
