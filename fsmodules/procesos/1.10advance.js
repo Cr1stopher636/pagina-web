898ddf2fcf228a9a8e0058142a159af5e83833fb
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-06 20:39:28 -0600

    edited: file

--- fsmodules/procesos/1.10advance.js
+++ fsmodules/procesos/1.10advance.js
@@ -1,84 +1,45 @@
-console.log(process.arch)
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
 
 
+const files = process.argv[2] ?? '.';
 
 
+// async function ls(files) {
+    let files;
+    try {
+        files = await fs.readdir(files)
+    } catch {
+        console.error('Error al leer', files)
+        process.exit(1)
+    }
 
 
+    const filePromises = files.map(async elemento => {
+        const filePath = path.join(files, elemento)
+        let Stat;
+        try {
+            Stat = await fs.stat(filePath)
+        } catch {
+            console.error('no se pudo', filePath)
+            process.exit(1)
+        }
 
+        const isDirectory = Stat.isDirectory()
+        const fileType = Stat.isDirectory ? 'd' : '-'
+        const fileSize = Stat.size
+        const fileModified = Stat.mtime.toLocaleString()
 
+        return fileType, fileSize.toString(), fileModified, isDirectory
+    })
 
 
+    const fileInfo = await Promise.all(filePromises)
+    fileInfo.forEach(element => {
+        console.log(element)
+    })
 
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ls(files)
