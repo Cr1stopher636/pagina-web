1e89ab3512adaee339dce69607876ec9f82fae56
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-06 21:16:46 -0600

    fixed: ruta de directorio

--- fsmodules/procesos/1.12advance.js
+++ fsmodules/procesos/1.12advance.js
@@ -1,17 +1,30 @@
 const fs = require('node:fs/promises');
 
 const rutaDirectorio = process.argv[2] ?? '.';
+fs.readdir(rutaDirectorio)
+    .then(file => {
+        file.forEach(element => {
+            console.log('Salio 👍', element)
+        })
+    })
 
-async function init(directorio) {
-    try {
-        const files = await fs.readdir(directorio)
-        console.log(files);
+    .catch(err => {
+        if (err) {
+            console.log('Salio mal papi❌😔', err);
+            return;
+        }
+    })
 
-    } catch (err) {
-        console.error('Error al leer el directorio', err)
-        console.error(err.message);
-        process.exit(1);
-    }
-}
+// async function init(directorio) {
+//     try {
+//         const files = await fs.readdir(directorio)
+//         console.log(files);
 
-init(rutaDirectorio);
+//     } catch (err) {
+//         console.error('Error al leer el directorio', err)
+//         console.error(err.message);
+//         process.exit(1);
+//     }
+// }
+
+// init(rutaDirectorio);
