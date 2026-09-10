58c085047d71859bafe0218bdc6e868e737a286c
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-10 14:03:46 -0600

    add: software process.argv exercise

---
+++ fsmodules/procesos/1.15advance.js
@@ -0,0 +1,39 @@
+
+
+const fs = require('node:fs/promises');
+
+const file = process.argv[2];
+
+
+async function init(directorio) {
+    if (!file) {
+        console.error(' Error: Debes proporcionar la ruta de un directorio.');
+        console.log('Uso: node app.js <ruta-del-directorio>');
+        process.exit(1);
+    }
+
+    try {
+
+        const stats = await fs.stat(directorio)
+
+        if (!stats.isDirectory()) {
+             console.error('La ruta proporcionada no es un directorio');
+             process.exit(1);
+        }
+
+        const variable = await fs.readdir(directorio)
+        
+        variable.map(element => {
+            console.log('Todo salio correcto Papi✅', element);
+        })
+
+
+        // Aquí puedes continuar procesando tus archivos...
+
+    } catch (err) {
+        console.error('Error al leer el directorio❌❌❌');
+        process.exit(1);
+    }
+};
+
+init(file);
