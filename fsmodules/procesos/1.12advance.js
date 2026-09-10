ccbf1a10c2b4d04dd7b42daa9767ea31be6763fd
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-10 14:04:01 -0600

    changes: general

--- fsmodules/procesos/1.12advance.js
+++ fsmodules/procesos/1.12advance.js
@@ -1,30 +1,33 @@
 const fs = require('node:fs/promises');
 
 const rutaDirectorio = process.argv[2] ?? '.';
+
 fs.readdir(rutaDirectorio)
     .then(file => {
-        file.forEach(element => {
+        file.map(element => {
             console.log('Salio 👍', element)
         })
     })
 
     .catch(err => {
-        if (err) {
-            console.log('Salio mal papi❌😔', err);
-            return;
-        }
+        console.log('Salio mal papi❌😔', err);
+        return;
     })
 
-// async function init(directorio) {
-//     try {
-//         const files = await fs.readdir(directorio)
-//         console.log(files);
+// async function init() {
+  
 
-//     } catch (err) {
-//         console.error('Error al leer el directorio', err)
-//         console.error(err.message);
-//         process.exit(1);
-//     }
 // }
+
+// init();
+
+
+
+
+// console.error('Error al leer el directorio', err)
+// console.error(err.message);
+// process.exit(1);
+
+
 
 // init(rutaDirectorio);
