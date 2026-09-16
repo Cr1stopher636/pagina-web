75dcf794f5e9a320e361801d7a68820b605972c3
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-16 12:16:07 -0600

    Formas de crear y ejecutar una promise

--- practica.js
+++ practica.js
@@ -1,36 +1,59 @@
-const fs = require('node:fs/promises');
-
-const promesa = new Promise((resolve, reject) => {
-    console.log('Se leyo el archivo')
-    console.log()
-})
 
 
+// 1forma de ejecutar crear y ejecutar una promesa EXPLICITA
 
+// const miPromesa = new Promise((resolve, reject) => {
+//     let variable = true;
 
-
-
-
-
-
-
-
-
-
-
-
-
-// .then(files => {
-//     files.forEach(files => {
-//         console.log(files);
-//     })
+//     if (variable) {
+//     resolve("¡Todo salió bien!"); // Se cumple
+//   } else {
+//     reject("Hubo un error"); // Falla
+//   }
 // })
-// .catch(err => {
-//     if (err) {
-//         console.log('Error al leer el directorio', err)
-//         return;
+
+// console.log(miPromesa);
+
+
+// 2forma de ejecutar crear y ejecutar una promesa con .THEN / .CATCH 
+
+// fs.readFile('script.js', 'utf-8')
+//     .then(items => {
+//         console.log('Este es el contenido✅', '\n', items);
+//     })
+
+//     .catch(err => {
+//         console.error('Error al leer el archivo...');
+//     })
+
+
+
+
+// 3era forma de crear y ejecutar promesas ASYNC/AWAIT
+
+// async function init() {
+//     try {
+//         const file = await fs.readFile('./script.js', 'utf-8');
+//         console.log('Contenido:✅', '\n', file);
+//     } catch (err) {
+//         console.error('Error al leer el archivo');
 //     }
-// });
+// }
+
+// init();
+
+
+
+
+// 4rta forma de crear y ejecutar promesas CALLBACKS
+
+// const fs = require('node:fs');
+
+// fs.readFile('script.js', 'utf-8', (err, text) => {
+//     console.log('Contenido:✅', '\n', text);
+// })
+
+
 
 
 
