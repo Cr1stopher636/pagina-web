c2c3886f4d93b422bc0f167aad9ab7d152d1ad7d
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-17 08:57:24 -0600

    add: freeport

---
+++ miServer/freePort.js
@@ -0,0 +1,25 @@
+const { resolve } = require('node:dns');
+const net = require('node:net');
+
+function fineavaliblePort(desirePort) {
+    return new Promise((resolve, reject) => {
+        const server = net.createServer()
+
+        server.listen(desirePort, () => {
+            const { port } = server.address()
+            server.close(() => {
+                resolve(port)
+            })
+        })
+        server.on('error', (err) => {
+            if (err.code === 'EADDRINUSE') {
+                fineavaliblePort(desirePort + 1).then(port => resolve(port))
+
+            } else {
+                reject(err)
+            }
+        })
+    })
+}
+
+module.exports = { fineavaliblePort }
