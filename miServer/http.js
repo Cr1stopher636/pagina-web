f039396695d51ba0daeae059b17f74bfd6e6d0c2
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-17 11:04:17 -0600

    add: coments

--- miServer/http.js
+++ miServer/http.js
@@ -1,23 +1,23 @@
-const http = require('node:http')
-const desirePort = process.env.PORT ?? 3000;
-const { fineavaliblePort } = require('./freePort')
-const server = http.createServer((req, res) => {
-    console.log('Solicitud aceptada✅');
-    res.end('Hola PapiRick')
-})
+// const http = require('node:http')
+// const desirePort = process.env.PORT ?? 3000;
+// const { fineavaliblePort } = require('./freePort')
+// const server = http.createServer((req, res) => {
+//     console.log('Solicitud aceptada✅');
+//     res.end('Hola PapiRick')
+// })
 
 
-fineavaliblePort(desirePort).then(port => {
-    server.listen(port, () => {
-        console.log('Server escuchando en ', port)
-    })
-})
+// fineavaliblePort(desirePort).then(port => {
+//     server.listen(port, () => {
+//         console.log('Server escuchando en ', port)
+//     })
+// })
 
 
 
-// setTimeout(() => {
-//     console.log('Han pasado 2 segundos !');
-// }, 2000);
+// // setTimeout(() => {
+// //     console.log('Han pasado 2 segundos !');
+// // }, 2000);
 
 
 
