a6d061e8c513f0912165cdb68a0160d80155945d
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-17 22:21:58 -0600

    changes: processRequest

--- miServer/http.js
+++ miServer/http.js
@@ -1,19 +1,35 @@
 const http = require('node:http')
 const desirePort = process.env.PORT ?? 3000;
-const { fineavaliblePort } = require('./freePort')
+// const { fineavaliblePort } = require('./freePort')
 
-const processRequest = ((req, res) => {
-    console.log('Solicitud aceptada✅', req.url);
-    res.end('Hola papiRick')
-})
+const processRequest = (req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('Bienvenido a la pagina de inicio')
+    }
+}
+
+
 
 const server = http.createServer(processRequest)
 
-fineavaliblePort(desirePort).then(port => {
-    server.listen(port, () => {
-        console.log(`Servidor escuchando en http://localhost${port}`);
-    })
+server.listen(desirePort, () => {
+    console.log(`Servidor escuchando en ${desirePort}`);
 })
+
+// const processRequest = ((req, res) => {
+//     console.log('Solicitud aceptada✅', req.url);
+//     res.end('Hola papiRick')
+// })
+
+// const server = http.createServer(processRequest)
+
+// fineavaliblePort(desirePort).then(port => {
+//     server.listen(port, () => {
+//         console.log(`Servidor escuchando en http://localhost${port}`);
+//     })
+// })
 
 
 
