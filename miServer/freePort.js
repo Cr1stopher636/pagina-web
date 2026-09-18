0dc79f8526c6946f15baf1a29838a7222b784b49
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-17 20:42:03 -0600

    changes: http server

--- miServer/freePort.js
+++ miServer/freePort.js
@@ -1,25 +1,24 @@
-// const { resolve } = require('node:dns');
-// const net = require('node:net');
+const net = require('node:net');
 
-// function fineavaliblePort(desirePort) {
-//     return new Promise((resolve, reject) => {
-//         const server = net.createServer()
+function fineavaliblePort(desirePort) {
+    return new Promise((resolve, reject) => {
+        const server = net.createServer()
 
-//         server.listen(desirePort, () => {
-//             const { port } = server.address()
-//             server.close(() => {
-//                 resolve(port)
-//             })
-//         })
-//         server.on('error', (err) => {
-//             if (err.code === 'EADDRINUSE') {
-//                 fineavaliblePort(desirePort + 1).then(port => resolve(port))
+        server.listen(desirePort, () => {
+            const { port } = server.address()
+            server.close(() => {
+                resolve(port)
+            })
+        })
+        server.on('error', (err) => {
+            if (err.code === 'EADDRINUSE') {
+                fineavaliblePort(desirePort + 1).then(port => resolve(port))
 
-//             } else {
-//                 reject(err)
-//             }
-//         })
-//     })
-// }
+            } else {
+                reject(err)
+            }
+        })
+    })
+}
 
-// module.exports = { fineavaliblePort }
+module.exports = { fineavaliblePort }
--- miServer/http.js
+++ miServer/http.js
@@ -2,23 +2,24 @@ const http = require('node:http')
 const desirePort = process.env.PORT ?? 3000;
 const { fineavaliblePort } = require('./freePort')
 
-const server = http.createServer((req, res) => {
-    console.log('Solicitud aceptada✅');
-    res.end('Hola PapiRick')
+const processRequest = ((req, res) => {
+    console.log('Solicitud aceptada✅', req.url);
+    res.end('Hola papiRick')
+})
+
+const server = http.createServer(processRequest)
+
+fineavaliblePort(desirePort).then(port => {
+    server.listen(port, () => {
+        console.log(`Servidor escuchando en http://localhost${port}`);
+    })
 })
 
 
-// fineavaliblePort(desirePort).then(port => {
-//     server.listen(port, () => {
-//         console.log('Server escuchando en ', port)
-//     })
-// })
 
-
-
-// // setTimeout(() => {
-// //     console.log('Han pasado 2 segundos !');
-// // }, 2000);
+// setTimeout(() => {
+//     console.log('Han pasado 2 segundos !');
+// }, 2000);
 
 
 
