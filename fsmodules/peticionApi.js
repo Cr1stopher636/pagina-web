989f07b50e6401e09d22ebd9f2f6427424a05b47
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-20 14:22:56 -0600

    add: json

--- fsmodules/peticionApi.js
+++ fsmodules/peticionApi.js
@@ -1,5 +1,5 @@
 fetch("http://localhost:3000")
-  .then(respuesta => respuesta.text()) // 1. Convertimos la respuesta a JSON
+  .then(respuesta => respuesta.json()) // 1. Convertimos la respuesta a JSON
   .then((datos) => {
     console.log("Datos recibidos:", datos); // 2. Usamos los datos procesados
   })
