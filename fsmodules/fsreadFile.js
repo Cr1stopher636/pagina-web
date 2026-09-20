396605107e4d2309c065612c3fb105d6ca2ab3d3
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-20 08:48:33 -0600

    add: callbacks readFile async

--- fsmodules/fsreadFile.js
+++ fsmodules/fsreadFile.js
@@ -2,20 +2,12 @@ const { readFile } = require('node:fs')
 
 console.log("leyendo el primer archivo...")
 readFile('./archivo2.txt', 'utf-8', (err, text) => {
-    if (err) {
-        console.log("Error al leer archivo2:", err);
-        return;
-    }
-    console.log("Primero:", text);
+    console.log(text);
 });
 
 console.log("leyendo el segundo archivo...")
 readFile('./archivo3.txt', 'utf-8', (err, text) => {
-    if (err) {
-        console.log("error al leer el archivo3", err);
-        return;
-    } 
-    console.log("Segundo:", text);
+    console.log(text);
 });
 
 
