2e35e3532b0789e77c818be05128ecd1743a62cc
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-20 13:58:19 -0600

    modified: fetch endpoint

---
+++ fsmodules/peticionApi.js
@@ -0,0 +1,8 @@
+fetch("http://localhost:3000")
+  .then(respuesta => respuesta.text()) // 1. Convertimos la respuesta a JSON
+  .then((datos) => {
+    console.log("Datos recibidos:", datos); // 2. Usamos los datos procesados
+  })
+  .catch((error) => {
+    console.error("Ocurrió un error:", error); // Captura fallos en cualquier punto de la cadena
+  });
