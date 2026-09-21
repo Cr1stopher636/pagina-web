54126148af5eb71ea37da4ed5141014377837074
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-21 15:01:07 -0600

    add: files

---
+++ fsmodules/ejercicios/consumoAPI.js
@@ -0,0 +1,32 @@
+// CONSUMIENDO UNA API con .then y catch
+
+// const obtenerInfo = fetch('http://localhost:3000')
+//     .then((datos) => datos.json())
+//     .then(datos => {
+//         console.log(datos);
+//     })
+
+//     .catch(err => {
+//         console.error('Error al consumir la API');
+//     })
+
+
+// CONSUMIENDO APIS CON ASYNC AWAIT
+
+// async function info() {
+//     const request = await fetch('http://localhost:3000/contacto')
+//     const files = await request.json()
+//     console.log(files);
+// }
+
+// info();
+
+
+
+
+
+
+
+
+
+
