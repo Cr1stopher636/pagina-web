e2b6cafae45f3dddd6197c63c7c0c8daf99c6300
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-21 12:12:48 -0600

    add: petición API

--- fsmodules/peticionApi.js
+++ fsmodules/peticionApi.js
@@ -1,8 +1,11 @@
-fetch("http://localhost:3000")
-  .then(respuesta => respuesta.json()) // 1. Convertimos la respuesta a JSON
-  .then((datos) => {
-    console.log("Datos recibidos:", datos); // 2. Usamos los datos procesados
-  })
-  .catch((error) => {
-    console.error("Ocurrió un error:", error); // Captura fallos en cualquier punto de la cadena
-  });
+fetch("http://localhost:3000/contacto")
+.then((respuesta) => respuesta.json())
+.then((datos) => {
+    console.log(datos);
+})
+
+.catch(err => {
+    console.error('Error al consumir la API❌');
+})
+
+
