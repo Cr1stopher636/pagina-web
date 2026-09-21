a522908728ac21d6ad8febe5875b15c7530793cb
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-21 16:50:57 -0600

    add: configurador y bandeja

--- miServer/http3.js
+++ miServer/http3.js
@@ -1,5 +1,4 @@
 const http = require('node:http');
-const fs = require('node:fs');
 const desirePort = 3000;
 
 const server = http.createServer((req, res) => {
@@ -21,18 +20,33 @@ const server = http.createServer((req, res) => {
     } else if (req.url === '/modelos') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json; charset=utf-8');
-        const primerObjeto = [{ id: 1, nombre: 'ricardo' }]
+        const primerObjeto = { id: 1, nombre: 'ricardo' }
         res.end(JSON.stringify(primerObjeto))
 
     } else if (req.url === '/contacto') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        const segundaLista =
-            `   <h2>Segundo contenido de la página de autos de lujo</h2>
+        const segundaLista = 
+            `<h2>Segundo contenido de la página de autos de lujo</h2>
             <p>Somos una empresa de autos de reventa de lujo</p>`
 
         res.end(segundaLista)
-    } else {
+
+    } else if (req.url === '/bandeja') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        const segundoObjeto = {id: 2, nombre: 'andrea'};
+        res.end(JSON.stringify(segundoObjeto));
+        // nombreEl.textContent = datos[0].nombre;
+    } else if (req.url === '/configurador') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        const variable = `<h1>este es el configurador de autos</h1>`;
+        res.end(variable)
+    }
+    
+    
+    else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain; charset=utf-8');
         res.end('404-Página no encontrada')
