0291a82e0cf20a7996b9683ccc6231d30a75868d
Cr1stopher636 <ricardo0640@outlook.com>
2026-09-22 15:43:12 -0600

    add: cabeceras server

--- miServer/htpp.js
+++ miServer/htpp.js
@@ -1,10 +1,17 @@
 const http = require('node:http')
 
+const desirePort = 3001;
+
+
 const server = http.createServer((req, res) => {
-    console.log('respuesta recibida')
-    res.end('hola mundo')
+    if (req.url === '/') {
+        console.log('Solicitud recibida✅')
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        const miObjeto = {id: 1, nombre: 'juan'}
+        res.end(JSON.stringify(miObjeto.nombre));
+    }
 })
 
-server.listen(51123, () => {
-    console.log(server.address().port)
+server.listen(desirePort, () => {
+    console.log('Servidor corriendo en el puerto:', desirePort);
 })
